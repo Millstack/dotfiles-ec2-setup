c1374e6591861418df25df0f733bff8229264ae2
Milind Khamkar <fullstackwithmilind@gmail.com>
2026-02-13 11:17:53 +0530

    aliases setup

---
+++ aliases.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Navigation
+alias ..="cd .."
+alias ...="cd ../.."
+
+# Listing
+alias ll="ls -lahF --color=auto"
+alias l="ls -CF"
+alias lst="ls -ltr"
+
+# Git Shortcuts
+alias gst="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+
+# Cloud/DevOps
+alias myip="curl ifconfig.me"
+alias tf="terraform"
+alias k="kubectl"
